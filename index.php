472b372afd906653f0e0a8538317e23b058229c5
aaronlague <aaronlague@yahoo.com>
2014-04-25 14:26:00 +0800

    updated footer for mobile devices
    updated footer for mobile devices

--- index.php
+++ index.php
@@ -175,15 +175,21 @@ Mobilyser makes record keeping a breeze and empowers the user with exact figures
 </section>
 <section class="footerSection">
   <div class="container">
-    <div class="footerContainer col-lg-10 col-lg-push-1">
-      <footer>
-        <div class="copyrightNote col-lg-5 col-xs-5"> <span>&copy; Copyright 2014 Mobilyser.  All rights reserved.</span> </div>
-        <div class="footerNavs col-lg-3 col-xs-3 col-lg-offset-1" style="padding-left:0!important; margin-left:7.333333%!important;"> <img src="images/image-footer-logo-hp.png" border="0"> </div>
-        <div class="footerNavs col-lg-1 col-xs-1"> <a href="#">Home</a> </div>
-        <div class="footerNavs col-lg-1 col-xs-1"> <a href="#">Privacy</a> </div>
-        <div class="footerNavs col-lg-1 col-xs-1"> <a href="#">Terms</a> </div>
-      </footer>
-    </div>
+    <div class="row">
+		<footer>
+			<div class="footerNavs col-lg-5 col-md-6 col-sm-12 col-xs-12 main pull-right">
+				<div class="col-lg-2 col-md-2"><a href="#">Home</a> </div>
+				<div class="col-lg-2 col-md-2"><a href="#">Privacy</a> </div>
+				<div class="col-lg-2 col-md-2"><a href="#">Terms</a> </div>
+			</div>
+			<div class="copyrightNote col-lg-offset-1 col-lg-5 col-md-6 col-sm-12 col-xs-12 side pull-left">
+				<span>&copy; Copyright 2014 Mobilyser.  All rights reserved.</span>
+			</div>
+			<div class="footerNavs footerIcon col-lg-1 col-md-3 col-sm-12 col-xs-12 side pull-left" style="">
+				<img src="images/image-footer-logo-hp.png" border="0">
+			</div>
+		</footer>
+	</div>
   </div>
 </section>
 <div class="overlaySection"></div>
