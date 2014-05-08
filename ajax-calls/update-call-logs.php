f6d56f13976d9af29815e705815edfffb537142c
aaronlague <aaronlague@yahoo.com>
2014-05-08 10:37:28 +0800

    call history toggle switch update
    call history toggle switch update
    - fixed the sql error updating the contact list on AJAX

--- ajax-calls/update-call-logs.php
+++ ajax-calls/update-call-logs.php
@@ -12,7 +12,7 @@ $caller_tag = $db->escape($_POST['caller_tag']);
 $phone = $db->escape($_POST['phone']);
 
 $db->mquery("UPDATE call SET caller_tag = '".$caller_tag."' WHERE REPLACE(REPLACE(phone_number, ' ', ''),'+','')='".$phone."'", $connect);
-$contact_db->mquery("UPDATE Contacts SET Caller_Tag = '".$caller_tag."' WHERE REPLACE(REPLACE(Phonenumber, ' ', ''),'+',''))='".$phone."'", $connect);
+$contact_db->mquery("UPDATE Contacts SET Caller_Tag = '".$caller_tag."' WHERE REPLACE(REPLACE(Phonenumber, ' ', ''),'+','')='".$phone."'", $connect);
 
 //check data values
 //echo $caller_tag;
